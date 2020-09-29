71fb2f9badc0732b96c767805f6b6da33d620ad0
Kelly Maisel <maiselkelly@gmail.com>
2020-09-29 17:45:02 -0400

    First Commit Kelly Maisel

---
+++ src/main/java/com/roomie/RoomieAppApplication.java
@@ -0,0 +1,63 @@
+package com.roomie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@Controller
+public class RoomieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomieAppApplication.class, args);
+	}
+
+	@GetMapping("/home")
+	public String homePage(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return "HomePage";
+	}
+
+	@GetMapping("/Registration")
+	public String registration(){
+		return "Registration";
+	}
+
+	@GetMapping("/LogIn")
+	public String logIn(){
+		return "LogIn";
+	}
+
+	@GetMapping("/RoomiezMatches")
+	public String potentialMatches(){
+		return "PotentialMatches";
+	}
+
+	@GetMapping("/SearchByLocation")
+	public String searchLocation(){
+		return "SearchLocation";
+	}
+
+	@GetMapping("/SearchNewMembers")
+	public String searchNewMembers(){
+		return "SearchNewMember";
+	}
+
+	@GetMapping("/SearchOnline")
+	public String searchOnline(){
+		return "SearchOnline";
+	}
+
+	@GetMapping("/SearchRecommended")
+	public String searchRecommended(){
+		return "SearchRecommended";
+	}
+
+	@GetMapping("/UserProfile")
+	public String userProfile(){
+		return "UserProfile";
+	}
+
+}
---
+++ src/test/java/com/roomie/RoomieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
