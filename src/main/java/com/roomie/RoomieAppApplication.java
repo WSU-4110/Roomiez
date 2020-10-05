15fb235c0c85fde07828389396996f02c202ca3e
Kelly Maisel <maiselkelly@gmail.com>
2020-10-05 12:32:38 -0400

    Adding User Homepage html

--- src/main/java/com/roomie/RoomieAppApplication.java
+++ src/main/java/com/roomie/RoomieAppApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -60,6 +61,10 @@ public class RoomieAppApplication {
 		return "UserProfile";
 	}
 
+	@GetMapping("/UserHomePage")
+	public String userHomePage(){
+		return "UserHomePage";
+	}
 
 
 }
